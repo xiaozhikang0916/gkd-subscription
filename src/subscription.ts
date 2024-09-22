25eca91dad90c7121cc9edaa65b5d1030b350d0f
xiaozhikang <xiaozhikang0916@gmail.com>
2024-09-22 20:33:32 +0800

    feat(build): update subscription id

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,7 +4,7 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 7400916,
   name: 'Subscription',
   version: 0,
   author: 'author',
