fcea4e81300991e65195fcbcee51a87f0f07f5c2
xiaozhikang <xiaozhikang0916@gmail.com>
2025-02-26 10:39:14 +0800

    feat(rule): add ecitic back

---
+++ src/apps/com.ecitic.bank.mobile.ts
@@ -0,0 +1,18 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ecitic.bank.mobile',
+  name: '中信银行',
+  groups: [
+    {
+      key: 1,
+      name: '开启消息通知',
+      desc: '自动点击"取消"',
+      actionMaximum: 1,
+      activityIds: '.ui.MainActivity',
+      fastQuery: true,
+      rules: '[id="com.ecitic.bank.mobile:id/alert_cancel"]',
+      snapshotUrls: 'https://i.gkd.li/i/18975994',
+    },
+  ],
+});
